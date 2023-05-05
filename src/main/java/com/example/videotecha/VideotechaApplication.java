6318792c9feafec3ba2371354a8473a005c129b8
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2023-05-05 14:04:14 +0200

    Project initialization

---
+++ src/main/java/com/example/videotecha/VideotechaApplication.java
@@ -0,0 +1,13 @@
+package com.example.videotecha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotechaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotechaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/videotecha/VideotechaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videotecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
