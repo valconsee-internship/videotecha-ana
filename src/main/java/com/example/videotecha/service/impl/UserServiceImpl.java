953d17f90c366ddd413efd4a0813b07527474d74
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2023-05-09 11:11:28 +0200

    Get all users functionality implemented

--- src/main/java/com/example/videotecha/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/videotecha/service/impl/UserServiceImpl.java
@@ -1,19 +1,23 @@
-package com.example.videotecha.service.hashMapImpl;
+package com.example.videotecha.service.impl;
 
-
-import com.example.videotecha.VideotechaApplication;
 import com.example.videotecha.model.RegisteredUser;
+import com.example.videotecha.repository.RegisteredUserRepository;
 import com.example.videotecha.service.UserService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    RegisteredUserRepository registeredUserRepository;
+
+    public UserServiceImpl(RegisteredUserRepository registeredUserRepository) {
+        this.registeredUserRepository = registeredUserRepository;
+    }
+
     @Override
     public List<RegisteredUser> getAllUsers() {
-        return new ArrayList<RegisteredUser>(VideotechaApplication.users.values());
+        return registeredUserRepository.findAll();
     }
 }
