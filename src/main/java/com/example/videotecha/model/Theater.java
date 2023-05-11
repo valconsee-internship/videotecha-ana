fa9e393751cfd264ac5defa70c1c638d893ddd28
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2023-05-11 10:04:48 +0200

    [VAL-3] Theater model and data  added

---
+++ src/main/java/com/example/videotecha/model/Theater.java
@@ -0,0 +1,52 @@
+package com.example.videotecha.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Theater {
+
+    @Id
+    @SequenceGenerator(name = "theaterSequenceGenerator", sequenceName = "theaterSequenceGenerator", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "theaterSequenceGenerator")
+    @Column(name="id", unique=true, nullable=false)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private int numberOfSeats;
+
+    public Theater() {}
+
+    public Theater(String name, int numberOfSeats) {
+        this.name = name;
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "Theater{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", numberOfSeats=" + numberOfSeats +
+                '}';
+    }
+}
--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,6 +1,13 @@
 
--- registered user
+-- users
 INSERT INTO public.users (id, email, first_name, last_name, "password", "role", username)
 VALUES (nextval('user_sequence_generator'), 'admin@gmail.com', 'Admin', 'Admin', '123', 'ADMIN', 'admin'),
        (nextval('user_sequence_generator'), 'ana@valcon.com', 'Ana', 'Gavrilovic', '123', 'USER', 'ana'),
        (nextval('user_sequence_generator'), 'sanja@gmail.com', 'Sanja', 'Drinic', '123', 'USER', 'sanja');
+
+--  theatres
+INSERT INTO public.theater(id, "name", number_of_seats)
+VALUES (nextval('theater_sequence_generator'), 'Grand Room', 50),
+       (nextval('theater_sequence_generator'), 'Indie Room', 10),
+       (nextval('theater_sequence_generator'), 'Couple Room', 2);
+
