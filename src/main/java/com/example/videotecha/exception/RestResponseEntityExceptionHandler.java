e8391d456335eb6e0f1ff5d12cb003111f5b654f
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2023-05-22 11:19:37 +0200

    [VAL-8] Exception handling fix

---
+++ src/main/java/com/example/videotecha/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.videotecha.exception;
+
+import com.example.videotecha.dto.ErrorDto;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.Timestamp;
+
+@RestControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ObjectAlreadyExistsException.class,
+                       BusinessLogicException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorDto handleBadRequestException(RuntimeException e) {
+        return new ErrorDto(e.getMessage(), HttpStatus.BAD_REQUEST, new Timestamp(System.currentTimeMillis()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected ErrorDto handleNotFoundException(EntityNotFoundException e) {
+        return new ErrorDto(e.getMessage(), HttpStatus.NOT_FOUND, new Timestamp(System.currentTimeMillis()));
+    }
+
+}
